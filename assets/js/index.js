f476ca3743932ce61f65c4a03e1f8c384b82411d
Ellisonxiao <ellisonxiao@foxmail.com>
2022-05-30 20:35:01 +0800

    index

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+function getUserinfo() {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    // headers: { Authorization: localStorage.getItem("token") },
+    success: function (res) {
+    //   console.log(res);
+      if(res.status!==0)  return layer.msg('获取用户信息失败！');
+
+      layer.msg("获取用户信息成功！");
+      renderAvatar(res.data)
+    },
+  });
+}
+function renderAvatar(user){
+    let name = user.nickname || user.username;
+    $("#welcome").html(`欢迎 ${name}`);
+    if (user.user_pic !== null){
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    }else{
+        $(".layui-nav-img").hide();
+        let firstName = name[0].toUpperCase();
+        $(".text-avatar").html(firstName);
+    }
+}
+getUserinfo()
+// 退出登录
+$("#btnLogout").click(() => {
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+  });
